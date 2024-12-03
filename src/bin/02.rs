d67e12b421a0283499f49788e4d6de4c31fc120b
Emin <xeome@proton.me>
2024-12-03 20:08:28 +0300

    Optimize day02

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,51 +1,65 @@
 advent_of_code::solution!(2);
 
 fn is_safe(numbers: &[i32]) -> bool {
-    if numbers.len() < 2 {
-        return true;
+    numbers.len() < 2 || {
+        let mut diffs = numbers.windows(2).map(|w| w[1] - w[0]);
+        let first_diff = diffs.next().unwrap();
+        if (1..=3).contains(&first_diff) {
+            diffs.all(|d| (1..=3).contains(&d))
+        } else if (-3..=-1).contains(&first_diff) {
+            diffs.all(|d| (-3..=-1).contains(&d))
+        } else {
+            false
+        }
     }
-    let diffs: Vec<_> = numbers.windows(2).map(|w| w[1] - w[0]).collect();
-    let all_positive = diffs.iter().all(|&d| (1..=3).contains(&d));
-    let all_negative = diffs.iter().all(|&d| (-3..0).contains(&d));
-    all_positive || all_negative
 }
 
+#[inline]
 fn is_safe2(numbers: &[i32]) -> bool {
     if is_safe(numbers) {
         return true;
     }
 
-    (0..numbers.len()).any(|i| {
-        let mut modified = numbers.to_vec();
-        modified.remove(i);
-        is_safe(&modified)
-    })
+    let mut modified = Vec::with_capacity(numbers.len());
+
+    for i in 0..numbers.len() {
+        modified.clear();
+        modified.extend(numbers[..i].iter());
+        modified.extend(numbers[i + 1..].iter());
+
+        if is_safe(&modified) {
+            return true;
+        }
+    }
+    false
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let safe_count = input
-        .lines()
-        .map(|line| {
-            line.split_whitespace()
-                .filter_map(|n| n.parse::<i32>().ok())
-                .collect::<Vec<_>>()
-        })
-        .filter(|nums| is_safe(nums))
-        .count();
-    Some(safe_count as u32)
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                line.split_whitespace()
+                    .filter_map(|n| n.parse::<i32>().ok())
+                    .collect::<Vec<_>>()
+            })
+            .filter(|nums| is_safe(nums))
+            .count() as u32,
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let safe_count = input
-        .lines()
-        .map(|line| {
-            line.split_whitespace()
-                .filter_map(|n| n.parse::<i32>().ok())
-                .collect::<Vec<_>>()
-        })
-        .filter(|nums| is_safe2(nums))
-        .count();
-    Some(safe_count as u32)
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                line.split_whitespace()
+                    .filter_map(|n| n.parse::<i32>().ok())
+                    .collect::<Vec<_>>()
+            })
+            .filter(|nums| is_safe2(nums))
+            .count() as u32,
+    )
 }
 
 #[cfg(test)]
