96f2fb5b670481a8f7657dd681581aba620c0eb8
Emin <xeome@proton.me>
2024-12-03 20:26:52 +0300

    Optimize day03

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,11 +1,14 @@
+use lazy_static::lazy_static;
 use regex::Regex;
 
 advent_of_code::solution!(3);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    }
 
-    let sum: i32 = re
+    let sum: i32 = RE
         .captures_iter(input)
         .map(|cap| {
             let x: i32 = cap[1].parse().unwrap();
@@ -16,7 +19,7 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(sum as u32)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Token {
     Multiply(i32, i32),
     Enable,
@@ -24,25 +27,34 @@ enum Token {
 }
 
 fn parse_input(input: &str) -> Vec<Token> {
-    let mut tokens = Vec::new();
-    let re = Regex::new(r"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))").unwrap();
-
-    for cap in re.captures_iter(input) {
-        let token = match &cap[1] {
-            s if s.starts_with("mul") => {
-                let nums = re.captures(s).unwrap();
-                Token::Multiply(nums[2].parse().unwrap(), nums[3].parse().unwrap())
-            }
-            "do()" => Token::Enable,
-            "don't()" => Token::Disable,
-            _ => continue,
-        };
-        tokens.push(token);
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
     }
+
+    let estimated_size = input.len() / 10;
+    let mut tokens = Vec::with_capacity(estimated_size);
+
+    RE.find_iter(input).for_each(|m| {
+        let s = m.as_str();
+        match s {
+            s if s.starts_with("mul") => {
+                let mut nums = s[4..s.len() - 1].split(',');
+                if let (Some(x), Some(y)) = (nums.next(), nums.next()) {
+                    if let (Ok(x), Ok(y)) = (x.parse::<i32>(), y.parse::<i32>()) {
+                        tokens.push(Token::Multiply(x, y));
+                    }
+                }
+            }
+            "do()" => tokens.push(Token::Enable),
+            "don't()" => tokens.push(Token::Disable),
+            _ => {}
+        }
+    });
+
     tokens
 }
 
-fn calculate_sum(tokens: Vec<Token>) -> i32 {
+fn calculate_sum(tokens: &[Token]) -> i32 {
     let mut sum = 0;
     let mut enabled = true;
 
@@ -59,7 +71,7 @@ fn calculate_sum(tokens: Vec<Token>) -> i32 {
 
 pub fn part_two(input: &str) -> Option<u32> {
     let tokens = parse_input(input);
-    let sum = calculate_sum(tokens);
+    let sum = calculate_sum(&tokens);
     Some(sum as u32)
 }
 
